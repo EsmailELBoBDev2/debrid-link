df428d399d91d29b66ccf2506da3d12b5437e802
Esmail EL BoB <github.defilable@simplelogin.co>
2024-05-26 03:52:16 +0300

    Create qbittorrent.sh

---
+++ qbittorrent.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#######
+## TO Add this to qbittorrent GO TO WEB UI AND EDIT IT MANUALLY AND PASTE LOCAL IP IN SONARR/RADARR.
+
+## TODO: LOAD TORRENT LIST AND SEND UNQUIE ONE ONLY.
+## TODO: EVERY 2 WEEKS RE-INSERT ALL TORRENTS JUST IN CASE.
+#######
+
+## Login to qBittorrent and get the cookie value
+curl_output=$(curl -i --header 'Referer: http://localhost:8080' --data 'username=admin&password=adminadmin' http://localhost:8080/api/v2/auth/login)
+set_cookie_header=$(echo "$curl_output" | grep -i 'Set-Cookie' | tr -d '[:space:]' | sed 's/set-cookie://i')
+cookie_value=$(echo "$set_cookie_header" | cut -d ';' -f 1 | tr -d ';')
+
+## Retrieve torrents info
+json_output=$(curl -s "http://localhost:8080/api/v2/torrents/info" --cookie "$cookie_value")
+
+## Check if there was an error with the curl request
+if [ -z "$json_output" ]; then
+    echo "Error: Unable to retrieve torrents info."
+    exit 1
+fi
+
+### TO UPLOAD TO DEBRID LINK
+
+## Get yours: https://debrid-link.com/webapp/apikey
+API_KEY=""
+
+## Loop through megnet links and upload them to debrid-link
+echo "$json_output" | jq -r '.[] | .name, .magnet_uri' | while IFS= read -r name && IFS= read -r magnet_uri; do
+    FILE_URL="$magnet_uri"
+
+    ## Make an HTTP POST request to the seedbox-add endpoint
+    curl_output=$(curl -s -X POST "https://debrid-link.com/api/v2/seedbox/add" \
+        -H "Authorization: Bearer $API_KEY" \
+        -d "url=$FILE_URL")
+
+    ## Checks for errors like torrent is too big.
+    if echo "$curl_output" | jq -e '.success == false' >/dev/null; then
+        error=$(echo "$curl_output" | jq -r '.error')
+        error_id=$(echo "$curl_output" | jq -r '.error_id')
+        echo "$name : $magnet_uri - ($error - $error_id)"  >> failed_torrents.txt
+        echo "" >> failed_torrents.txt
+    else
+        # Process the response normally
+        echo "Successfully added: $name"
+    fi
+done
